4bdcec8cab9876ff18b5e8d3874cfbb9c9fb4917
chico_skater <josenatteschoque@gmail.com>
2024-09-06 15:10:23 -0300

    ejercicio15.c

--- practico1/ejercicio15.c
+++ practico1/ejercicio15.c
@@ -1,3 +1,12 @@
+/*Ejercicio 15. Escriba un programa que permita ingresar una letra L e imprima todas la letras del abecedario en
+orden alfabético desde la letra a ( o A) hasta L. Tenga en cuenta si el usuario ingresó una letra minúscula o
+mayúscula. (ver nota 3 en anexo).
+#Char: En el lenguaje C una variable char es similar a una variable int pero con la salvedad que puede
+almacenar un valor entero más chico (en el rango de -128 a 127), estos valores enteros positivos de las
+variables de tipo char, están relacionadas con la tabla de caracteres ASCII. y pueden realizarse
+comparaciones y operaciones aritméticas como si se trataran de un entero (Procurando permanecer en
+el rango definido por el tipo).*/
+
 #include <stdio.h>
 int main()
 {
