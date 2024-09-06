5ae124e956449606c7495c0368abae906a4aaa67
chico_skater <josenatteschoque@gmail.com>
2024-09-06 15:13:34 -0300

    ejercicio18.c

--- practico1/ejercicio18.c
+++ practico1/ejercicio18.c
@@ -1,3 +1,9 @@
+/*Ejercicio 18. Escriba un programa que permita ingresar un número entero N e indique si el número es capicúa.
+(ver nota 5 en anexo).
+#Número Capicúa: En matemáticas, la palabra capicúa (del catalán cap i cua, ‘cabeza y cola’)1 se refiere
+a cualquier número que se lee igual de izquierda a derecha que de derecha a izquierda. Ejemplos: 161,
+2992, 3003, 91019, 5005, 292, 2882, 2442, 9102019.*/
+
 #include <stdio.h>
 int main()
 {
