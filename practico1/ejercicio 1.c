c265d4888d0fbab084004a892badf5e9cb29f0d7
chico_skater <josenatteschoque@gmail.com>
2024-09-06 14:53:58 -0300

    ejercicio 1.c

--- practico1/ejercicio 1.c
+++ practico1/ejercicio 1.c
@@ -1,3 +1,5 @@
+/*Ejercicio 1. Escriba un programa que imprima en la terminal la cadena de texto “Hola mundo”. (utiliza un
+editor de texto plano para escribir el código fuente.)*/
 #include <stdio.h>
 int main()
 {
