e902407e4f0c2b1e12f471c4a308adc5cbdbc459
chico_skater <josenatteschoque@gmail.com>
2024-09-06 14:55:10 -0300

    ejercicio2.c

--- practico1/ejercicio2.c
+++ practico1/ejercicio2.c
@@ -1,3 +1,6 @@
+/*Ejercicio 2. Escriba un programa que imprima en la terminal la cadena de texto “¿Cuál es tu nombre?”,
+permita el ingreso mediante el teclado de una cadena almacenándola en una variable nombre y luego
+imprima en la terminal el mensaje “Hola nombre”.*/
 #include <stdio.h>
 #define max_length 100
 
