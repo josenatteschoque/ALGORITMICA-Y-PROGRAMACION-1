d5a18a5587dd410f587292d63c742a508c12d941
chico_skater <josenatteschoque@gmail.com>
2024-09-06 15:05:02 -0300

    ejercicio10.c

--- practico1/ejercicio10.c
+++ practico1/ejercicio10.c
@@ -1,3 +1,9 @@
+/*Ejercicio 10. Escriba un programa que permita ingresar un número natural N e imprima el factorial de N, es
+decir N!. (PS) (PE N=5). (ver nota 1 en anexo).
+#Factorial: El factorial de un entero positivo n, el factorial de n o n factorial se define en principio como
+el producto de todos los números enteros positivos desde 1 (es decir, los números naturales) hasta n.
+Por ejemplo: 5!=1×2×3×4×5=120.*/
+
 #include <stdio.h>
 int main()
 {
