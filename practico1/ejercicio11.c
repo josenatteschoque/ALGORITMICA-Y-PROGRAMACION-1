596094899a5565b9c97c434510eb279cfd47bfa0
chico_skater <josenatteschoque@gmail.com>
2024-09-06 15:06:20 -0300

    ejercicio11.c

--- practico1/ejercicio11.c
+++ practico1/ejercicio11.c
@@ -1,3 +1,9 @@
+/*Ejercicio 11. Escriba un programa que permita ingresar un número natural N e imprima los números primos
+menores o iguales a N. (PS) (PE N=10). (ver nota 2 en anexo).
+#Número Primo: En matemáticas, un número primo es un número natural mayor que 1 que tiene
+únicamente dos divisores positivos distintos: él mismo y el 1. Por ejemplo: 2, 3, 5, 7, 11, 13, 17, 19, 23,
+29, 31, 37, 41, 43, 47, 53…*/
+
 #include <stdio.h>
 int main()
 {
