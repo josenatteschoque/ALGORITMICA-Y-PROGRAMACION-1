ae9b6f096004bd82d125712695415aaae3f681ad
chico_skater <josenatteschoque@gmail.com>
2024-09-06 14:58:26 -0300

    ejercicio4.c

--- practico1/ejercicio4.c
+++ practico1/ejercicio4.c
@@ -1,3 +1,7 @@
+/*Ejercicio 4. Escriba un programa que pida al usuario que ingrese su edad y, si la edad es 18 o más, muestre el
+mensaje "Eres mayor de edad", de lo contrario, muestre "No eres mayor de edad". Utiliza una estructura
+if-then-else para lograr esto.*/
+
 #include <stdio.h>
 int main()
 {
